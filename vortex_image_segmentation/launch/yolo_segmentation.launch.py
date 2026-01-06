b63d01d33b40b04f6cc291b46425b86221bb8cc6
Mads Engesvoll <madsjorgen.be@gmail.com>
2026-01-06 12:04:05 +0000

    fix: use get_package_share_directory for non docker users

--- vortex_image_segmentation/launch/yolo_segmentation.launch.py
+++ vortex_image_segmentation/launch/yolo_segmentation.launch.py
@@ -1,16 +1,19 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('vortex_image_segmentation')
+    yolo_params = os.path.join(pkg_share, 'params', 'yolo_params.yaml')
+
     return LaunchDescription([
         Node(
             package='vortex_image_segmentation',
             executable='yolo_seg_node',
             name='yolo_segmentation_node',
             output='screen',
-            parameters=[
-                '/ros2_ws/vortex_image_segmentation/params/yolo_params.yaml'
-            ]
+            parameters=[yolo_params]
         )
     ])
